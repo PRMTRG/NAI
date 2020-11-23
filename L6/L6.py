843cf666fe34753471cebe3a024ef752950b5d3b
TM <mirski.tymoteusz@gmail.com>
2020-11-23 20:43:16 +0100

    Lab 6

--- L6/L6.py
+++ L6/L6.py
@@ -14,64 +14,129 @@ class TestFunction:
 
 
 class RandomSolver:
-    def __init__(self, test_function):
+    def __init__(self, test_function, name):
         self.test_function = test_function
-    def solve(self):
+        self.name = name
+    def generate_random_args(self):
         args = []
-        for arg in self.test_function.search_domain:
-            args.append(random.uniform(arg[0], arg[1]))
-        return args, self.test_function.goal(args)
-    def solve_n_times(self, n):
-        best_solution, best_result = self.solve()
-        for i in range(1, n):
-            new_solution, new_result = self.solve()
+        for d in self.test_function.search_domain:
+            args.append(random.uniform(d[0], d[1]))
+        return args
+    def solve(self, iterations, save_data_for_plot=False, plotting_step=1):
+        best_args = self.generate_random_args()
+        best_result = self.test_function.goal(best_args)
+        if save_data_for_plot:
+            plot_time = []
+            plot_time.append(0)
+            plot_result = []
+            plot_result.append(best_result)
+            start_time = time.perf_counter()
+        for i in range(1, iterations):
+            new_args = self.generate_random_args()
+            new_result = self.test_function.goal(new_args)
             if new_result < best_result:
-                best_solution = new_solution.copy()
+                best_args = new_args
                 best_result = new_result
-        return best_solution, best_result
-    def solve_n_times_and_save_data_for_plot(self, n, step):
-        plot_best_results = []
-        plot_time_taken = []
-        best_solution, best_result = self.solve()
-        plot_best_results.append(best_result)
-        plot_time_taken.append(0)
-        n -= 1
-        start_time = time.perf_counter()
-        while n > 0:
-            if n - step < 0:
-                step = n
-            n -= step
-            new_best_solution, new_best_result = self.solve_n_times(step)
-            if new_best_result < best_result:
-                best_solution = new_best_solution
-                best_result = new_best_result
-            plot_best_results.append(best_result)
-            plot_time_taken.append(time.perf_counter() - start_time)
-        return best_solution, best_result, plot_best_results, plot_time_taken
-    def solve_n_times_m_times_and_plot(self, n, m, step, plot_title):
-        best_results_list = []
-        time_taken_list = []
-        plot_results = []
+            if save_data_for_plot and (i % plotting_step == 0 or i == iterations - 1):
+                plot_time.append(time.perf_counter() - start_time)
+                plot_result.append(best_result)
+        if save_data_for_plot:
+            return best_args, best_result, plot_time, plot_result
+        else:
+            return best_args, best_result
+    def average_out_and_plot(self, list_plot_time, list_plot_result):
+        n = len(list_plot_time[0])
+        m = len(list_plot_time)
         plot_time = []
-        for i in range(m):
-            _, _, best_results, time_taken = self.solve_n_times_and_save_data_for_plot(n, step)
-            best_results_list.append(best_results)
-            time_taken_list.append(time_taken)
-        n = math.ceil(n / step)
+        plot_result = []
         for i in range(n):
             s1 = 0
             s2 = 0
             for j in range(m):
-                s1 += best_results_list[j][i]
-                s2 += time_taken_list[j][i]
-            plot_results.append(s1 / m)
+                s1 += list_plot_result[j][i]
+                s2 += list_plot_time[j][i]
+            plot_result.append(s1 / m)
             plot_time.append(s2 / m)
-        plt.plot(plot_time, plot_results)
-        plt.title(plot_title)
+        plt.plot(plot_time, plot_result)
+        plt.title(self.name + " - Random")
         plt.xlabel('Time')
         plt.ylabel('Result')
         plt.show()
-        
+    def solve_multiple_times_and_plot(self, iterations, experiments, plotting_step=1):
+        list_plot_time = []
+        list_plot_result = []
+        for i in range(experiments):
+            _, _, plot_time, plot_result = self.solve(iterations, save_data_for_plot=True, plotting_step=plotting_step)
+            list_plot_time.append(plot_time)
+            list_plot_result.append(plot_result)
+        self.average_out_and_plot(list_plot_time, list_plot_result)
+
+
+class RandomClimbSolver:
+    def __init__(self, test_function, name):
+        self.test_function = test_function
+        self.name = name
+    def generate_random_args(self):
+        args = []
+        for d in self.test_function.search_domain:
+            args.append(random.uniform(d[0], d[1]))
+        return args
+    def generate_random_neighbour(self, args):
+        new_args = []
+        for i in range(len(args)):
+            domain = self.test_function.search_domain[i]
+            max_distance_half = abs(domain[0] - domain[1]) / 100
+            new_args.append(args[i] + random.uniform(-max_distance_half, max_distance_half))
+        return new_args
+    def solve(self, iterations, save_data_for_plot=False, plotting_step=1):
+        best_args = self.generate_random_args()
+        best_result = self.test_function.goal(best_args)
+        if save_data_for_plot:
+            plot_time = []
+            plot_time.append(0)
+            plot_result = []
+            plot_result.append(best_result)
+            start_time = time.perf_counter()
+        for i in range(1, iterations):
+            new_args = self.generate_random_neighbour(best_args)
+            new_result = self.test_function.goal(new_args)
+            if new_result < best_result:
+                best_args = new_args
+                best_result = new_result
+            if save_data_for_plot and (i % plotting_step == 0 or i == iterations - 1):
+                plot_time.append(time.perf_counter() - start_time)
+                plot_result.append(best_result)
+        if save_data_for_plot:
+            return best_args, best_result, plot_time, plot_result
+        else:
+            return best_args, best_result
+    def average_out_and_plot(self, list_plot_time, list_plot_result):
+        n = len(list_plot_time[0])
+        m = len(list_plot_time)
+        plot_time = []
+        plot_result = []
+        for i in range(n):
+            s1 = 0
+            s2 = 0
+            for j in range(m):
+                s1 += list_plot_result[j][i]
+                s2 += list_plot_time[j][i]
+            plot_result.append(s1 / m)
+            plot_time.append(s2 / m)
+        plt.plot(plot_time, plot_result)
+        plt.title(self.name + " - Random Climb")
+        plt.xlabel('Time')
+        plt.ylabel('Result')
+        plt.show()
+    def solve_multiple_times_and_plot(self, iterations, experiments, plotting_step=1):
+        list_plot_time = []
+        list_plot_result = []
+        for i in range(experiments):
+            _, _, plot_time, plot_result = self.solve(iterations, save_data_for_plot=True, plotting_step=plotting_step)
+            list_plot_time.append(plot_time)
+            list_plot_result.append(plot_result)
+        self.average_out_and_plot(list_plot_time, list_plot_result)
+
 
 def generate_rastring_function(n):
     def rastring_formula(x):
@@ -119,21 +184,30 @@ def generate_booth_function():
 
 
 if __name__ == "__main__":
-    rastring_formula, rastring_global_optimum, rastring_search_domain = generate_rastring_function(10)
-    rastring_random_solver = RandomSolver(TestFunction(rastring_formula, rastring_global_optimum, rastring_search_domain))
     
-    sphere_formula, sphere_global_optimum, sphere_search_domain = generate_sphere_function(10)
-    sphere_random_solver = RandomSolver(TestFunction(sphere_formula, sphere_global_optimum, sphere_search_domain))
+    functions = []
+    functions.append([ generate_rastring_function(2), "Rastring function" ])
+    functions.append([ generate_sphere_function(2), "Sphere function" ])
+    functions.append([ generate_beale_function(), "Beale function" ])
+    functions.append([ generate_booth_function(), "Booth function" ])
     
-    beale_formula, beale_global_optimum, beale_search_domain = generate_beale_function()
-    beale_random_solver = RandomSolver(TestFunction(beale_formula, beale_global_optimum, beale_search_domain))
+    random_solvers = []
+    for f in functions:
+        test_function = TestFunction(f[0][0], f[0][1], f[0][2])
+        random_solver = RandomSolver(test_function, f[1])
+        random_solvers.append(random_solver)
+        
+    random_climb_solvers = []
+    for f in functions:
+        test_function = TestFunction(f[0][0], f[0][1], f[0][2])
+        random_climb_solver = RandomClimbSolver(test_function, f[1])
+        random_climb_solvers.append(random_climb_solver)
     
-    booth_formula, booth_global_optimum, booth_search_domain = generate_booth_function()
-    booth_random_solver = RandomSolver(TestFunction(booth_formula, booth_global_optimum, booth_search_domain))
+    for rs in random_solvers:
+        rs.solve_multiple_times_and_plot(100, 20, plotting_step=1)
     
-    rastring_random_solver.solve_n_times_m_times_and_plot(100, 20, 1, "Rastring function")
-    sphere_random_solver.solve_n_times_m_times_and_plot(100, 20, 1, "Sphere function")
-    beale_random_solver.solve_n_times_m_times_and_plot(100, 20, 1, "Beale function")
-    booth_random_solver.solve_n_times_m_times_and_plot(100, 20, 1, "Booth function")
+    for rcs in random_climb_solvers:
+        rcs.solve_multiple_times_and_plot(100, 20, plotting_step=1)
+
     
     
